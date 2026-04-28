6715e8859353217a39f36b0bce664055710e6786
zhixindev <zhixin.zhu@qq.com>
2026-04-28 10:11:32 +0800

    chore: fix cloudflare build error

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from "astro/config";
+import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
+  adapter: cloudflare(),
   site: process.env.PUBLIC_SITE_URL || undefined,
   output: "static"
 });
